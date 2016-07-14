3361430afc7e9377b796f97c71347aa0ba740b76
Ow Zhiyin8 <owzhiyin@gmail.com>
2016-07-14 16:18:09 +0800

    Initial Commit

---
+++ PryceIt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PryceIt
+//
+//  Created by Ow Zhiyin on 14/7/16.
+//  Copyright © 2016 Ow Zhiyin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
